4a1c19224acefce835ed8b84f5c526cd3706e8da
Gerson <gedacaba@gmail.com>
2022-07-02 00:34:01 -0500

    agregandodiferentes graficas de casos de covid

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/about">COVID-19</router-link> |
       <router-link to="/api">Api</router-link>
     </nav>
     <router-view/>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,71 @@
 <template>
-<div class="container">
-  <div class="row mt-5" v-if="arrPositivo.length > 0">
-    <div class="col">
-      <h2>positivo</h2>
-      <linea-chart :chartData="arrPositivo" :options="chartOptions" :label="positive" ></linea-chart>
+  <div class="container">
+    <div class="row">
+      <div class="col text-center">
+        <h1>COVID 19 vizualización</h1>
+      </div>
+    </div>
+    <div class="row mt-5" v-if="arrPositivo.length > 0">
+      <div class="col">
+        <h2>casos positivo</h2>
+        <linea-chart
+          :chartData="arrPositivo"
+          :options="chartOptions"
+          :label="Positivo"
+        ></linea-chart>
+      </div>
+    </div>
+    <div class="row mt-5" v-if="arrNegativo.length > 0">
+      <div class="col">
+        <h2>casos negativo</h2>
+        <linea-chart
+          :chartData="arrNegativo"
+          :options="chartOptions"
+          :label="Negativo"
+        ></linea-chart>
+      </div>
+    </div>
+    <div class="row mt-5" v-if="arrHospitalizacion.length > 0">
+      <div class="col">
+        <h2>Hospitalizados</h2>
+        <linea-chart
+          :chartData="arrHospitalizacion"
+          :options="chartOptions"
+          :label="Hospitalizados"
+        ></linea-chart>
+      </div>
+    </div>
+    <div class="row mt-5" v-if="arrEnUsiActualmente.length > 0">
+      <div class="col">
+        <h2>En USI</h2>
+        <linea-chart
+          :chartData="arrEnUsiActualmente"
+          :options="chartOptions"
+          :label="inIcuCurrently"
+        ></linea-chart>
+      </div>
+    </div>
+    <div class="row mt-5" v-if="arrOnVentilador.length > 0">
+      <div class="col">
+        <h2>Casos reportados</h2>
+        <linea-chart
+          :chartData="arrOnVentilador"
+          :options="chartOptions"
+          :label="onVentilatorCurrently"
+        ></linea-chart>
+      </div>
+    </div>
+    <div class="row mt-5" v-if="arrMuertes.length > 0">
+      <div class="col">
+        <h2>Muertes</h2>
+        <linea-chart
+          :chartData="arrMuertes"
+          :options="chartOptions"
+          :label="death"
+        ></linea-chart>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -17,7 +76,7 @@ import { Bar } from "vue-chartjs/legacy";
 
 import LineaChart from "./LineaChart.vue";
 //registrar todos los elementos
-import 'chart.js/auto'
+import "chart.js/auto";
 import {
   Chart as ChartJS,
   Title,
@@ -36,7 +95,7 @@ ChartJS.register(
   BarElement,
   CategoryScale,
   LinearScale,
-  PointElement,
+  PointElement
 );
 
 export default {
@@ -50,41 +109,42 @@ export default {
       arrEnUsiActualmente: [],
       arrOnVentilador: [],
       arrMuertes: [],
-      chartOptions: { 
+      chartOptions: {
         responsive: true,
-        maintainAspectRatio: false
+        maintainAspectRatio: false,
       },
     };
   },
   async created() {
-            //console.log(response.data);
-        try {
-          const res = await axios.get("https://api.covidtracking.com/v1/us/daily.json")
-          await res.data.forEach(d => {
-              const date = moment(d.date, "YYYYMMDD").format("MM/DD/YY");
+    //console.log(response.data);
+    try {
+      const res = await axios.get(
+        "https://api.covidtracking.com/v1/us/daily.json"
+      );
+      await res.data.forEach((d) => {
+        const date = moment(d.date, "YYYYMMDD").format("MM/DD/YY");
 
-              const {
-                positive,
-                negative,
-                hospitalizedCurrently,
-                inIcuCurrently,
-                onVentilatorCurrently,
-                death,
-              } = d;
-            
-              this.arrPositivo.push({ date, total: positive });
-              this.arrNegativo.push({ date, total: negative });
-              this.arrHospitalizacion.push({ date, total: hospitalizedCurrently });
-              this.arrEnUsiActualmente.push({ date, total: inIcuCurrently });
-              this.arrOnVentilador.push({ date, total: onVentilatorCurrently });
-              this.arrMuertes.push({ date, total: death });
+        const {
+          positive,
+          negative,
+          hospitalizedCurrently,
+          inIcuCurrently,
+          onVentilatorCurrently,
+          death,
+        } = d;
 
-              //console.log(this.arrMuertes);
+        this.arrPositivo.push({ date, total: positive });
+        this.arrNegativo.push({ date, total: negative });
+        this.arrHospitalizacion.push({ date, total: hospitalizedCurrently });
+        this.arrEnUsiActualmente.push({ date, total: inIcuCurrently });
+        this.arrOnVentilador.push({ date, total: onVentilatorCurrently });
+        this.arrMuertes.push({ date, total: death });
 
-          })
-        } catch (error) {
-          console.log(error)
-        }
+        //console.log(this.arrMuertes);
+      });
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
 </script>
