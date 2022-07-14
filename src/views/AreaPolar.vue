8cef75b9e7024224c34756c8174543cceefb8f6a
Gerson Calvo <gedacaba@gmail.com>
2022-07-14 09:40:03 -0500

    estilos al area polar

--- src/views/AreaPolar.vue
+++ src/views/AreaPolar.vue
@@ -76,11 +76,11 @@ export default {
         datasets: [
           {
             label: 'My First dataset',
-            backgroundColor: 'rgba(179,181,198,0.2)',
-            pointBackgroundColor: 'rgba(179,181,198,1)',
+            backgroundColor: 'rgba(175,8,8,0.2)',
+            pointBackgroundColor: 'rgba(232,32,32,1)',
             pointBorderColor: '#fff',
             pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(179,181,198,1)',
+            pointHoverBorderColor: 'rgba(177,21,21,1)',
             data: [65, 59, 90, 81, 56, 55, 40]
           },
           {
